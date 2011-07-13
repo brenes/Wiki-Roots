fed135dde879b6ff40614b401188139ae07cf311
David J. Brenes <david.brenes@simplelogica.net>
2011-07-13 18:14:08 +0200

    Empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WikiRoots::Application.config.secret_token = 'ee49c4125bfedd48e1053a2f620841fa7e31e20316c552046bac2bae098301a4f8eede0e3db3eefa6af10e000d6be1f4f8b2ebfd36e4162d4bb0aff7181dfd39'
