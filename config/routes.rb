45ee862b9c5e2848dcbddd225b3f341b517a45f4
David J. Brenes <davidjbrenes@gmail.com>
2011-09-08 21:36:02 +0200

    Routing details for wiki trees controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ WikiRoots::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   resources :wiki_pages, :except => [:destroy]
+  resources :wiki_trees, :only => [:index, :show]
 
   # Sample resource route with options:
   #   resources :products do
@@ -48,7 +49,7 @@ WikiRoots::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'wiki_trees#index'
 
   # See how all your routes lay out with "rake routes"
 
